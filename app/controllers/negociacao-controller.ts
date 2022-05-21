125a22f491a74e48bd5220f21ef3859b2b9be171
Graciela Parente <graciparentte@gmail.com>
2022-05-21 20:29:35 -0300

    create optional parameters

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -1,3 +1,4 @@
+import { DiasDaSemana } from '../enums/dias-da-semana.js';
 import { Negociacao } from '../models/negociacao.js';
 import { Negociacoes } from '../models/negociacoes.js';
 import { MensagemView } from '../views/mensagem-view.js';
@@ -8,10 +9,8 @@ export class NegociacaoController {
   private inputQuantidade: HTMLInputElement;
   private inputValor: HTMLInputElement;
   private negociacoes = new Negociacoes();
-  private negociacoesView = new NegociacoesView('#negociacoesView');
+  private negociacoesView = new NegociacoesView('#negociacoesView', true);
   private mensagemView = new MensagemView('#mensagemView');
-  private readonly SABADO = 6;
-  private readonly DOMINGO = 0;
 
   //não tipamos constructor
   constructor() {
@@ -23,7 +22,11 @@ export class NegociacaoController {
 
   //método
   public adiciona(): void {
-    const negociacao = this.criaNegociacao();
+    const negociacao = Negociacao.criaDe(
+      this.inputData.value,
+      this.inputQuantidade.value,
+      this.inputValor.value
+    );
     if (!this.ehDiaUtil(negociacao.data)) {
       this.mensagemView.update('Apenas negociações em dias úteis são aceitas');
       return;
@@ -31,14 +34,6 @@ export class NegociacaoController {
     this.negociacoes.adiciona(negociacao);
     this.limparFormulario();
     this.atualizaView();
-  }
-
-  private criaNegociacao(): Negociacao {
-    const exp = /-/g;
-    const date = new Date(this.inputData.value.replace(exp, ','));
-    const quantidade = parseInt(this.inputQuantidade.value);
-    const valor = parseFloat(this.inputValor.value);
-    return new Negociacao(date, quantidade, valor);
   }
 
   private limparFormulario(): void {
@@ -54,6 +49,9 @@ export class NegociacaoController {
   }
 
   private ehDiaUtil(data: Date) {
-    return data.getDay() > this.DOMINGO && data.getDay() < this.SABADO;
+    return (
+      data.getDay() > DiasDaSemana.DOMINGO &&
+      data.getDay() < DiasDaSemana.SABADO
+    );
   }
 }
--- app/views/view.ts
+++ app/views/view.ts
@@ -2,14 +2,23 @@ export abstract class View<T> {
   //classe abstrata precisa que a classe filha a instancie
 
   protected elemento: HTMLElement;
+  private escapar: boolean = false;
   //protected deixa que os 'filhos' tenham acesso
 
-  constructor(seletor: string) {
+  constructor(seletor: string, escapar?: boolean) {
     this.elemento = document.querySelector(seletor);
+    if (escapar) {
+      this.escapar = escapar;
+      //o ? torna o escapar opcional, junto com essa condicional.
+      //não pode ser o primeiro parâmetro
+    }
   }
 
-  update(model: T): void {
-    const template = this.template(model);
+  public update(model: T): void {
+    let template = this.template(model);
+    if (this.escapar) {
+      template = template.replace(/<script>[\s\S]*?<script>/, '');
+    }
     this.elemento.innerHTML = template;
   }
 
